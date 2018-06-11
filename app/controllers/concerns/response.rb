9b63d4a2bfa1a0364c0fbf945d70217fb3a91ac0
Chris Staikos <cstaikos@gmail.com>
2018-06-11 09:19:16 -0400

    Fill out manuscript controller for CRUD actions

---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,6 @@
+module Response
+  extend ActiveSupport::Concern
+  def json_response(object, status=:ok)
+    render json: object, status: status
+  end
+end
--- app/controllers/manuscripts_controller.rb
+++ app/controllers/manuscripts_controller.rb
@@ -1,6 +1,45 @@
 class ManuscriptsController < ApplicationController
+  include Response
+
   def index
     @manuscripts = Manuscript.all
-    render json: @manuscripts, status: :ok
+    json_response(@manuscripts)
+  end
+
+  def show
+    @manuscript = Manuscript.find(params[:id])
+    json_response(@manuscript)
+  end
+
+  def create
+    @manuscript = Manuscript.create(manuscript_params)
+    if @manuscript.save
+      json_response(@manuscript, :created)
+    else
+      json_response({error: "Error creating manuscript"}, :bad_request)
+    end
+  end
+
+  def update
+    @manuscript = Manuscript.find(params[:id])
+    @manuscript.update(manuscript_params)
+    if @manuscript.save
+      json_response(@manuscript)
+    else
+      json_response({error: "Error updating manuscript"}, :bad_request)
+    end
+  end
+
+  def destroy
+    @manuscript = Manuscript.find(params[:id])
+    if @manuscript.destroy
+      json_response({})
+    else
+      json_response({error: "Error deleting manuscript"}, :bad_request)
+    end
+  end
+
+  def manuscript_params
+    params.require(:manuscript).permit(:title)
   end
 end
