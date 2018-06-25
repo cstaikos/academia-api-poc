3c76b8bae883af7d8ae23c14d4ad27e3c7f68fc6
Travis Russell <travisrussell@Traviss-MacBook-Pro.local>
2018-06-24 20:09:14 -0400

    Added user model and authentication stuff

--- Gemfile
+++ Gemfile
@@ -46,3 +46,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'jwt'
+gem 'bcrypt', '~> 3.1.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_203116) do
+ActiveRecord::Schema.define(version: 2018_06_24_230324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 2018_06_21_203116) do
     t.string "content_content_type"
     t.integer "content_file_size"
     t.datetime "content_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "admin", default: false
+    t.boolean "active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
